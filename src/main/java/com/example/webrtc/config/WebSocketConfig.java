1d4a5fa1f4ca717024406f5ac88eb709f81ea46c
Jinkalu <vhichvishnu@gmail.com>
2025-02-13 18:13:58 +0530

    project structure

--- src/main/java/com/example/webrtc/config/WebSocketConfig.java
+++ src/main/java/com/example/webrtc/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.example.webrtc;
+package com.example.webrtc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
